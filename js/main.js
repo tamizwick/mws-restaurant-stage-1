fcfab11830070045522e07864b67fef0aa0f6ce8
Tami Zwick <tzwick@factsmgt.com>
2019-02-13 12:40:41 -0600

    Adds Mapbox API Token

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 let restaurants,
   neighborhoods,
-  cuisines
-var newMap
-var markers = []
+  cuisines;
+var newMap;
+var markers = [];
 
 /**
  * Fetch neighborhoods and cuisines as soon as the page is loaded.
@@ -78,7 +78,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-    mapboxToken: '<your MAPBOX API KEY HERE>',
+    mapboxToken: 'pk.eyJ1IjoidHp3aWNrIiwiYSI6ImNqczJidDd6ZjE3OWszeW1nNWtoMDQ2ZjYifQ.ObHmWOwNkWxIVlB6jWkx9Q',
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
