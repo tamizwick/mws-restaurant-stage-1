758fd5f8a6cb6a24c7a4dcbfc1b702e8698390d9
Tami Zwick <tzwick@factsmgt.com>
2019-02-19 20:34:08 -0600

    Beautifies JavaScript

--- sw.js
+++ sw.js
@@ -1,30 +1,30 @@
 let currentCache = 'restaurant-review-v5'
 
 self.addEventListener('install', function(event) {
-   event.waitUntil(
-       caches.open(currentCache).then(function(cache) {
-           return cache.addAll([
-               '/',
-               'css/styles.css',
-               'data/restaurants.json',
-               'img/1.jpg',
-               'img/2.jpg',
-               'img/3.jpg',
-               'img/4.jpg',
-               'img/5.jpg',
-               'img/6.jpg',
-               'img/7.jpg',
-               'img/8.jpg',
-               'img/9.jpg',
-               'img/10.jpg',
-               'js/dbhelper.js',
-               'js/main.js',
-               'js/restaurant_info.js',
-               'index.html',
-               'restaurant.html'
-           ]);
-       })
-   );
+    event.waitUntil(
+        caches.open(currentCache).then(function(cache) {
+            return cache.addAll([
+                '/',
+                'css/styles.css',
+                'data/restaurants.json',
+                'img/1.jpg',
+                'img/2.jpg',
+                'img/3.jpg',
+                'img/4.jpg',
+                'img/5.jpg',
+                'img/6.jpg',
+                'img/7.jpg',
+                'img/8.jpg',
+                'img/9.jpg',
+                'img/10.jpg',
+                'js/dbhelper.js',
+                'js/main.js',
+                'js/restaurant_info.js',
+                'index.html',
+                'restaurant.html'
+            ]);
+        })
+    );
 });
 
 self.addEventListener('activate', function(event) {
@@ -34,7 +34,7 @@ self.addEventListener('activate', function(event) {
             return Promise.all(
                 cacheNames.filter(function(cacheName) {
                     return cacheName.startsWith('restaurant-review') && cacheName != currentCache;
-                }).map(function (cacheName) {
+                }).map(function(cacheName) {
                     return caches.delete(cacheName);
                 })
             );
